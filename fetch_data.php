2f111457e0f59bbc4c86dfd827c8a77d7f28e57b
gautham <desertncamel@gmail.com>
2024-11-06 22:28:31 +0530

    updated prompt and formatted output

--- fetch_data.php
+++ fetch_data.php
@@ -6,9 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // The alternative method includes replacing the below line with your api key in quotes.
         // $apiKey = getenv("GEM_API_KEY");
+
         $apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
 
-        $message = "give me a recipe to make $name as a html webpage element.if $name is not a food item, or not something that has a recipe presay, return an appropriate error. the response should NOT be enclosed in a html code block (i.e. DO NOT use ```html anywhere in the code). use tailwind css to design the element. Note that it is to be included in an already existing html page, so format accordingly, do not use any background colors";
+        $message = "Generate HTML for a proper recipe card with nutrition facts in a table on the right side for $name as a webpage element. If $name is not a food item or has no recipe, return an error message. The output should be plain HTML without any ```html or code block formatting, and should only include the raw HTML without any additional explanations or markdown. use tailwind css to design the element. Note that it is to be included in an already existing html page, so format accordingly, do not use any background colors.";
 
 
         $data = json_encode([
@@ -36,6 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if (isset($responseArray['candidates'][0]['content']['parts'][0]['text'])) {
             $text = $responseArray['candidates'][0]['content']['parts'][0]['text'];
+            $text = str_replace(["```html", "```"], "", $text);
             header("Location: display.php?response=" . urlencode($text));
 
         } else {
